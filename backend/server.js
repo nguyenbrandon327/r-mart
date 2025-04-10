271c103ec28bfd57c947689ffc40d8e1625b6be8
brandonnguyen27 <nguyenbrandon327@gmail.com>
2025-04-09 19:03:17 -0700

    Set up backend and database connection

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // helmet is a middleware that helps secure the app by setting various HTTP headers
+app.use(morgan("dev")); // morgan is a middleware that logs the HTTP requests
+
+app.use("/api/products", productRoutes);
+
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                description TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.error("Error connecting to database", error);
+    }
+}
+ 
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
