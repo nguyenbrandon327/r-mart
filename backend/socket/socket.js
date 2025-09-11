550ac5d582e243b125a16a5b5b79b07cbcfae1d7
brandonnguyen27 <nguyenbrandon327@gmail.com>
2025-09-10 23:29:46 -0700

    Migrated from AWS to Heroku for cost savings

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -3,6 +3,7 @@ import http from "http";
 import express from "express";
 
 const app = express();
+app.set("trust proxy", 2);
 const server = http.createServer(app);
 
 const io = new Server(server, {
